18d99bafc9f852959d03d736d28be3422fa62c2f
samandargofurov <samandargofurov236@gmail.com>
2024-01-14 11:51:46 +0500

    1-lesson

---
+++ index.js
@@ -0,0 +1,119 @@
+// 1-masala
+// Berilgan sonning musbat yoki manfiyligini tekshiradigan algoritm yozing.
+// Bunda agar musbat bolsa musbat aks holda manfiy degan yozuvc chiqishi kerak.
+
+// let n = -12;
+// if (n > 0) {
+//     console.log("musbat son");
+// } else {
+//     console.log("manfiy son");
+// };
+
+// 2-masala
+// Berilgan sonning gaysi oraliqqa tegishli ekanini aniqlovchi algoritm yozing.
+
+// A: 98-100
+
+// B: 80-89
+
+// C: 70-79
+
+// D: 60-69
+
+// F: 0-59
+
+// Agar berilgan son 90 va 100 orasida bolsa A, 80 va 89 orasida bolsa B 60-69 orasida bolsa
+// D kabi yozuvlar chiqishi kerak
+
+// let a = 75;
+
+// if (a >= 98 && a <= 100) {
+//     console.log("A");
+// }
+// if (a >= 80 && a <= 89) {
+//     console.log("B");
+// }
+// if (a >= 70 && a <= 79) {
+//     console.log("C");
+// }
+// if (a >= 60 && a <= 69) {
+//     console.log("D");
+// }
+// if (a >= 0 && a <= 59) {
+//     console.log("F");
+// }
+
+
+// 3-masala
+// Berilgan yil kabisa yili ekanligini tekshiradigan funktsiyani yozing.
+// Kabisa yili 4 ga bo'linadi, lekin 100 ga bo'linmaydi, agar u 400 ga bo'linmasa.
+
+// 4-masala
+// Berilgan sonning manfiy, musbat yoki nolga teng ekanligini aniqlovchi algoritm yozing
+
+// let a = -12;
+
+// if (a > 0) {
+//     console.log("musbat son");
+// } else if (a < 0) {
+//     console.log("manfiy son");
+// } else {
+//     console.log("nolga teng son");
+// };
+
+// 5-masala
+// 2 ta sondan kattasini aniqlovchi algoritm yozing
+
+// let a = 8;
+// let b = 50;
+// let max;
+
+// if (a >= b) {
+//     max = a
+// } else {
+//     max = b
+// }
+
+// console.log(max);
+
+// 6-masala
+// 3 ta sondan kichigini aniqlovchi algoritm yozing
+
+// let a = 8;
+// let b = 5;
+// let c = 6;
+// let min;
+
+// if (a <= b) {
+//     min = a
+// } else {
+//     min = b
+// }
+// if (min <= c) {
+//     min = min
+// } else {
+//     min = c
+// }
+
+// console.log(min);
+
+// 7-masala
+// 3 ta son berilganda ular orasidan manfiylarining yigindisini topuvchi algoritm yozing
+
+// let a = -2;
+// let b = -5;
+// let c = 10;
+
+// let sum = 0;
+
+// if (a < 0) {
+//     sum += a
+// }
+// if (b < 0) {
+//     sum += b
+// }
+// if (c < 0) {
+//     sum += c
+// }
+
+// console.log(sum);
